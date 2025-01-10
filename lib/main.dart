8f1afdb5087c64f1a9ee51fc08b45cdad33b2d95
miz <mizu710tr@gmail.com>
2025-01-10 21:32:34 +0900

    下にタブを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/chat_screen.dart';
 import 'screens/ranking_screen.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,21 +19,38 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final _pageWidget = [ChatScreen(), RankingScreen()];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("みんなで親父ギャグ"),
-          bottom: TabBar(tabs: [
-            Tab(icon: Icon(Icons.list), text: "投稿リスト"),
-            Tab(icon: Icon(Icons.star), text: "ランキング"),
-          ]),
-        ),
-        body: TabBarView(children: [ChatScreen(), RankingScreen()]),
-      ),
+          appBar: AppBar(
+            title: Text("みんなで親父ギャグ"),
+          ),
+          body: _pageWidget.elementAt(_currentIndex),
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.chat), label: "ホーム"),
+              BottomNavigationBarItem(icon: Icon(Icons.list), label: "ランキング"),
+            ],
+            currentIndex: _currentIndex,
+            fixedColor: Colors.blueAccent,
+            type: BottomNavigationBarType.fixed,
+            onTap: (int index) {
+              setState(() {
+                _currentIndex = index; // タップされたインデックスに更新
+              });
+            },
+          )),
     );
   }
 }
